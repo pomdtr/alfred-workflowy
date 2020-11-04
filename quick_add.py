23f8f96651ad89c49ccf9fbe1611a3983d74f6d4
pomdtr <achille.lacoin@gmail.com>
2020-11-04 09:34:36 +0100

    remove unecessary logic from quick-add

--- quick_add.py
+++ quick_add.py
@@ -1,20 +1,8 @@
 import argparse
 import os
-import re
-from re import MULTILINE
 from uuid import uuid4
 
-import requests
-from urlparse import urlparse
 from workflowy_api.transport import Operation, Transport
-
-
-def uri_validator(uri):
-    try:
-        result = urlparse(uri)
-        return all([result.scheme, result.netloc, result.path])
-    except Exception:
-        return False
 
 
 if __name__ == "__main__":
@@ -23,24 +11,10 @@ if __name__ == "__main__":
     parser.add_argument("text")
     args = parser.parse_args()
 
-    text = args.text
-    if uri_validator(args.text):
-        try:
-            res = requests.get(args.text)
-            res.raise_for_status()
-            match = re.search(
-                "<title>(?:\\W+)(\w.+\w)(?:\\W+)</title>",
-                res.text,
-                re.MULTILINE | re.IGNORECASE,
-            ).group(1)
-            text = '<a href="{}">{}</a>'.format(args.text, match)
-        except Exception:
-            pass
-
     node_id = str(uuid4())
     operations = [
         Operation.create(node_id, args.parent),
-        Operation.edit_name(node_id, text),
+        Operation.edit_name(node_id, args.text),
     ]
 
     session_id = os.getenv("session_id")
